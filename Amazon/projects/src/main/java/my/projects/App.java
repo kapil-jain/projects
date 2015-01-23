f5bf43d62a20908f0ac73a34443f8187663dc4ca
kapil-jain <yes_kapil@hotmail.com>
2015-01-22 19:25:51 -0600

    Signed-off-by: kapil-jain <yes_kapil@hotmail.com>

---
+++ Amazon/projects/src/main/java/my/projects/App.java
@@ -0,0 +1,18 @@
+package my.projects;
+
+import java.util.Scanner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Scanner in = new Scanner(System.in);
+        System.out.println(in.next());
+        	
+    }
+}
